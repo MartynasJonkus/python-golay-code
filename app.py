445ff4481e1735763c467fb9f2dd0f58fc4336a7
MartynasJonkus <martynez00@gmail.com>
2024-11-11 18:07:13 +0200

    changed name of scenario file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from screens.simple_scenario import MainScreen
+from screens.main_scenario import MainScreen
 from screens.text_scenario import TextScreen
 from screens.image_scenario import ImageScreen
 
